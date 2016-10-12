649ba6bb4aca6021c28bbe972caf6e3b71369110
Joseph <joseph.jr@gmail.com>
2016-10-12 10:45:10 +0100

    linking frontend to backend

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require './lib/code_analyzer.rb'
 require './lib/repo_manager'
+require './lib/json_parser'
 class Recode < Sinatra::Base
 
   enable :sessions
@@ -34,8 +35,18 @@ class Recode < Sinatra::Base
   get '/repos/:id' do
     @file_list = RepoManager.new(session[:username])
     @file_list.repo_name = params[:id]
+    session[:repo_name] = params[:id]
     @file_list = @file_list.file_list
     erb(:file)
+  end
+
+  get '/analysis/:file' do
+    @content = RepoManager.new(session[:username])
+    @content.repo_name = session[:repo_name]
+    @content.file_name = params[:file]
+    @decoded_content = Json_parser.extract(@content.content)
+    @analysis = Code_analyzer.new(@decoded_content).analyse
+    erb(:analysis)
   end
 
 
